6744cbba0e33b3cddc8b440817fd59d960c2ad73
rediska0123 <rediska0123@gmail.com>
2020-11-08 22:10:50 +0300

    fix

--- camtrack/camtrack.py
+++ camtrack/camtrack.py
@@ -37,11 +37,11 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                           frame_sequence_path: str,
                           known_view_1: Optional[Tuple[int, Pose]] = None,
                           known_view_2: Optional[Tuple[int, Pose]] = None,
-                          max_reprojection_error: float = 1.0,
+                          max_reprojection_error: float = 7.5,
                           min_triangulation_angle_deg: float = 1.0,
                           min_depth: float = 0.1,
-                          max_retiangulation_points: int = 10,
-                          retriangulation_iters: int = 1000) -> Tuple[List[Pose], PointCloud]:
+                          min_retriangulation_frame_count: int = 10,
+                          min_inliers_for_retriangulation: int = 10) -> Tuple[List[Pose], PointCloud]:
     if known_view_1 is None or known_view_2 is None:
         raise NotImplementedError()
 
@@ -74,44 +74,75 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
     triangulate_and_add_points(corner_storage[known_view_1[0]], corner_storage[known_view_2[0]],
                                view_mats[known_view_1[0]], view_mats[known_view_2[0]])
 
-    def retriangulate(corner_id):
-        frame_ids = []
-        for i in range(len(view_mats)):
-            if view_mats[i] is None:
-                continue
-            if corner_id in corner_storage[i].ids:
-                frame_ids.append(i)
-        random.shuffle(frame_ids)
-        frame_ids = frame_ids[:min(len(frame_ids), max_retiangulation_points)]
-        if len(frame_ids) <= 10:
-            return None
-        best_inliers, best_point3d = [], None
+    def retriangulate(processed_frame_ids, iters=25):
+        if len(processed_frame_ids) < min_retriangulation_frame_count:
+            return
+        common_ids = corner_storage[processed_frame_ids[0]].ids.flatten()
+        for id in processed_frame_ids:
+            common_ids = snp.intersect(common_ids, corner_storage[id].ids.flatten())
+        retriangulation_points_num = len(common_ids)
+        if retriangulation_points_num == 0:
+            return
 
-        def point2d_by_frame_id(ы):
-            return corner_storage[ы].points[corner_storage[ы].ids.index(corner_id)]
+        points_positions = np.zeros((0, retriangulation_points_num, 2), dtype=float)
+        for id in processed_frame_ids:
+            _, (_, p_ids) = snp.intersect(common_ids, corner_storage[id].ids.flatten(), indices=True)
+            points_positions = np.concatenate((points_positions,
+                                               np.expand_dims(corner_storage[id].points[p_ids], axis=0)),
+                                              axis=0)
 
-        for _ in range(retriangulation_iters):
-            i, j = random.choice(len(frame_ids)), random.choice(len(frame_ids))
-            if i == j:
-                continue
-            point3d, ids, median_cos = triangulate_correspondences(
-                Correspondences(
-                    np.array([corner_id]),
-                    np.array([point2d_by_frame_id(i)]),
-                    np.array([point2d_by_frame_id(j)])),
-                view_mats[i], view_mats[j], intrinsic_mat, triang_params)
-            inliers = []
-            for frame_id in frame_ids:
-                if compute_reprojection_errors(point3d,
-                                               point2d_by_frame_id([frame_id]),
-                                               intrinsic_mat @ view_mats[frame_id])[0] \
-                        < max_reprojection_error:
-                    inliers.append(frame_id)
-            if len(best_inliers) <= len(inliers):
-                best_inliers, best_point3d = inliers, point3d
-        return best_point3d
+        frames_num = len(processed_frame_ids)
 
-    time_since_last_retriangulation = defaultdict(lambda: 1e9)
+        # Find best points3d with ransac
+        best_points3d, best_inliers_num, best_errs = np.full((retriangulation_points_num, 3), np.inf), \
+                                                     np.full((retriangulation_points_num,), 0), \
+                                                     np.full((retriangulation_points_num,), -np.inf)
+        inliers_mask = np.full((frames_num, retriangulation_points_num), False)
+
+        view_mat_projs = np.array([intrinsic_mat @ view_mats[id] for id in processed_frame_ids])
+
+        def svd(eqs):
+            u, s, vh = np.linalg.svd(np.stack(eqs, axis=1))
+            return vh[:, -1, :][:, :3] / vh[:, -1, :][:, [-1]]
+
+        def calculate_points3d(id1, id2):
+            p1 = points_positions[random_ids1, np.arange(retriangulation_points_num)]
+            p2 = points_positions[random_ids2, np.arange(retriangulation_points_num)]
+            eqs = [view_mat_projs[id][:, 2] * points[:, [h]] - view_mat_projs[id][:, h]
+                   for h in [0, 1] for id, points in [(id1, p1), (id2, p2)]]
+            return svd(eqs)
+
+        # Ransac iterations
+        for k in range(iters):
+            random_ids = np.array([np.random.choice(range(frames_num), 2, replace=False)
+                                   for _ in range(retriangulation_points_num)])
+            random_ids1, random_ids2 = random_ids[:, 0], random_ids[:, 1]  # random points for ransac iteration
+            points3d = calculate_points3d(random_ids1, random_ids2)
+            errs = np.array([compute_reprojection_errors(points3d,
+                                                         points_positions[i],
+                                                         intrinsic_mat @ view_mats[frame_id])
+                             for i, frame_id in enumerate(processed_frame_ids)])
+            cur_inliers_mask = errs < max_reprojection_error
+            inliers_num = np.count_nonzero(cur_inliers_mask, axis=0)
+            avg_errs = np.mean(errs, axis=0)
+
+            update_mask = np.logical_and(best_inliers_num <= inliers_num, best_errs < avg_errs)
+            best_inliers_num[update_mask], best_errs[update_mask], best_points3d[update_mask] = \
+                inliers_num[update_mask], avg_errs[update_mask], points3d[update_mask]
+            inliers_mask[:, update_mask] = cur_inliers_mask[:, update_mask]
+
+        view_mat_projs = np.repeat(view_mat_projs[:, np.newaxis], retriangulation_points_num, axis=1)
+        view_mat_projs[np.logical_not(inliers_mask), :, :] = np.zeros(view_mat_projs.shape[2:])
+
+        inliers = np.count_nonzero(inliers_mask, axis=0) > min_inliers_for_retriangulation
+        points = points_positions[:, inliers]
+        eqs = [view_mat_projs[:, inliers][:, :, 2] * points[:, :, [h]] - view_mat_projs[:, inliers][:, :, h]
+               for h in [0, 1]]
+        points3d = svd(eqs)
+
+        point_cloud_builder.add_points(common_ids[inliers], points3d)
+
+    processed_frame_ids = []
     while True:
         random_ids = list(range(len(view_mats)))
         random.shuffle(random_ids)
@@ -124,15 +155,26 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                                                  indices=True)
             if len(common) <= 10:
                 continue
+
+            points3d = point_cloud_builder.points[ids1]
+            points2d = corner_storage[i].points[ids2]
             retval, rvecs, tvecs, inliers = cv2.solvePnPRansac(
-                point_cloud_builder.points[ids1],
-                corner_storage[i].points[ids2],
-                intrinsic_mat,
+                points3d, points2d, intrinsic_mat,
+                iterationsCount=108,
+                reprojectionError=max_reprojection_error,
                 distCoeffs=None,
-                flags=cv2.SOLVEPNP_ITERATIVE
-            )
+                confidence=0.999,
+                flags=cv2.SOLVEPNP_EPNP)
+            if retval:
+                retval, rvecs, tvecs = cv2.solvePnP(
+                    points3d[inliers], points2d[inliers], intrinsic_mat,
+                    rvec=rvecs, tvec=tvecs,
+                    useExtrinsicGuess=True,
+                    distCoeffs=None,
+                    flags=cv2.SOLVEPNP_ITERATIVE)
             if not retval:
                 continue
+
             print('Iteration {}/{}, processing {}th frame: {} inliers, {} points in point cloud'
                   .format(len([v for v in view_mats if v is not None]) - 1,
                           len(rgb_sequence) - 2, i, len(inliers),
@@ -149,22 +191,13 @@ def track_and_calc_colors(camera_parameters: CameraParameters,
                     triangulate_and_add_points(corner_storage[j], inlier_corners,
                                                view_mats[j], view_mats[i])
 
-            break
+            processed_frame_ids.append(i)
+            if len(processed_frame_ids) >= min_retriangulation_frame_count:
+                retriangulate(np.array(processed_frame_ids))
+                processed_frame_ids = []
+
         if not found_new_view_mat:
             break
-
-        # Retriangulate
-        for j in range(len(point_cloud_builder.ids)):
-            corner_id = point_cloud_builder.ids[j][0]
-            if time_since_last_retriangulation[corner_id] < len(rgb_sequence) / 3:
-                continue
-            new_pos = retriangulate(corner_id)
-            if new_pos is not None:
-                time_since_last_retriangulation[corner_id] = 0
-                point_cloud_builder.add_points(corner_id, new_pos)
-
-        for key in time_since_last_retriangulation.keys():
-            time_since_last_retriangulation[key] += 1
 
     for i in list(range(0, len(view_mats))) * 2:
         if view_mats[i] is None:
